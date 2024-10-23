65baf8dfaf930c708ed7d5f4daea645245ae4930
Hushkisses <ssms7733@naver.com>
2024-10-23 20:46:47 +0900

    .

---
+++ c#/11382.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        
+        string[] stringNumbers = input.Split(' ');
+        long[] numbers = Array.ConvertAll(stringNumbers, long.Parse);
+        long sum = 0;
+        
+        sum = numbers[0] + numbers[1] + numbers[2]; // 입력값이 3개로 정해져 있기 때문에 예외는 처리하지 않음
+
+        Console.WriteLine(sum);
+    }
+}
